31cca907ed3df5310c974986a52ce7fdd8ae31d7
Mahmoud Zakria <me@zakcoding.com>
2020-08-19 16:55:10 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+export default {
+  bind(el, binding, vnode) {
+    const character_pattern = new RegExp(/\D+/, "g");
+    const currencyCB = target => {
+      if (character_pattern.test(target.value)) {
+        target.value = target.value.replace(character_pattern, '');
+      }
+      const splitted = binding.arg && binding.arg.split('[');
+      const [currency, locale] = splitted;
+      const finalLocale = locale.replace(']', '');
+      
+      return target.value &&
+      Number(target.value).toLocaleString(finalLocale || navigator.language, {
+        style: "currency",
+        currency: currency || 'USD'
+      });
+      
+    }
+
+    el.value = currencyCB(binding);
+    el.addEventListener("blur", e => (e.target.value = currencyCB(e.target)));
+    el.addEventListener(
+      "focus",
+      e =>
+        (e.target.value =
+          e.target.value && Number(e.target.value.replace(/[^\d.]/g, "")))
+    );
+    el.addEventListener("input", e => {
+      if (character_pattern.test(e.target.value)) {
+        e.target.value = e.target.value.replace(character_pattern, "");
+      }
+      vnode.context.$data[binding.expression] = e.target.value;
+    });
+  }
+};
