6f31448758ec216e58885bac085931fcf155650f
Mahmoud Zakria <me@zakcoding.com>
2020-08-21 06:43:19 +0200

    added cjs version

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,17 @@ import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: __dirname + '/main.js',
-  output: {
-    file: 'dist/index.min.js',
-    format: 'es',
-  },
+  output: [
+    {
+      file: 'dist/index.min.js',
+      format: 'es',
+    },
+    {
+      file: 'dist/index.cjs.min.js',
+      format: 'cjs',
+      exports: 'default'
+    }
+  ],
   plugins: [
     terser(),
     cleaner({
