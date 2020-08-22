301b2bc7cffaf58a40022bdd88d3c50832202ddc
Mahmoud Zakria <me@zakcoding.com>
2020-08-23 01:57:16 +0200

    revert to toBe

--- test/examples.test.js
+++ test/examples.test.js
@@ -81,6 +81,6 @@ describe('Test directive', () => {
         input.trigger('input');
         input.setValue(amountVal);
         input.trigger('blur');
-        expect(input.element.value).toMatchObject(expectedVal);
+        expect(input.element.value).toBe(expectedVal);
     })
 })
