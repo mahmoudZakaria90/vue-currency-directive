4d6e6ca76002a77e30952d25da9d32c8fc256875
Mahmoud Zakria <me@zakcoding.com>
2020-08-28 06:21:41 +0200

    added test for dynamic args

--- test/examples.test.js
+++ test/examples.test.js
@@ -3,7 +3,7 @@ import vueCurrencyDirective from '../dist/index.min';
 
 
 describe('Test directive', () => {
-    it('Output with a "USD" currency by default and for locale configured browser language in case of no arguments passed.', () => {
+    it('Outputs with a "USD" currency by default and for locale configured browser language in case of no arguments passed.', () => {
         const amountVal = 3244;
         const expectedVal = '$3,244.00';
         const Component = {
@@ -30,7 +30,7 @@ describe('Test directive', () => {
         expect(input.element.value).toBe(expectedVal);
     })
 
-    it('Output with a "EUR" currency when currency:EUR arg is passed', () => {
+    it('Outputs with a "EUR" currency when currency:EUR arg is passed', () => {
         const amountVal = 100234;
         const expectedVal = '€100,234.00';
         const Component = {
@@ -57,7 +57,7 @@ describe('Test directive', () => {
         expect(input.element.value).toBe(expectedVal);
     })
 
-    it('Output with a "EUR" currency and french locale when currency:EUR[fr-FR] arg is passed', () => {
+    it('Outputs with a "EUR" currency and french locale when currency:EUR[fr-FR] arg is passed', () => {
         const replacePattern = /\s/g;
         const amountVal = 43434;
         const expectedVal = '43 434,00 €'.replace(replacePattern, '');
@@ -84,5 +84,63 @@ describe('Test directive', () => {
         input.trigger('blur');
         const receivedVal = input.element.value.replace(replacePattern, '')
         expect(receivedVal).toEqual(expectedVal);
+    })
+})
+
+describe('Dynamic arguments', () => {
+    it('Outputs with a dynamic currency/locale based on data()', () => {
+        const template = `
+                           <div>
+                                <input v-currency="amount.value">
+                                //Currency selector
+                                <select id="currency-select" v-model="amount.currency">
+                                    <option value="USD">USD</option>
+                                    <option value="EUR">EUR</option>
+                                    <option value="GBP">GBP</option>
+                                </select>
+                                
+                                //Locale selector
+                                <select id="locale-select" v-model="amount.locale">
+                                    <option value="en-US">US</option>
+                                    <option value="de-DE">DE</option>
+                                    <option value="en-GB">GB</option>
+                                </select>
+                           </div>
+                         `
+        const amountVal = 13232;
+        const expectedVal = '£13,232.00';
+        const Component = {
+            template,
+            directives: {
+                currency: vueCurrencyDirective
+            },
+        }
+        const wrapper = mount(Component, {
+            data() {
+                return {
+                    amount: {
+                        value: '',
+                        currency: 'USD',
+                        locale: 'en-US',
+                        formatted: ''
+                    }
+                }
+            },
+        })
+        const input = wrapper.find('input');
+        const currencySelect = wrapper.find('#currency-select');
+        const localeSelect = wrapper.find('#locale-select');
+
+        input.setValue(amountVal);
+
+        currencySelect.trigger('focus');
+        currencySelect.trigger('change');
+        currencySelect.setValue('GBP');
+
+        localeSelect.trigger('focus');
+        localeSelect.trigger('change');
+        localeSelect.setValue('en-GB');
+
+        expect(input.element.value).toBe(expectedVal);
     })
 })
