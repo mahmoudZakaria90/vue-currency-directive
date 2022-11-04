6a04d3a8cefbe4b235b1ca871f400e1959cbaa27
Mahmoud Ali <83590985+MahmoudAli-heycar@users.noreply.github.com>
2022-11-04 22:40:28 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import handleUpdate from './handleUpdate';
 export default {
   bind(el, binding, vnode) {
     const targetEl = el.tagName.toLowerCase() !== 'input' ? el.querySelector('.v-currency-input') || el.querySelector('input[type="text"]') || el.querySelector('input') : el;
-    const character_pattern = new RegExp(/[a-zA-z,\W]/, "g");
+    const character_pattern = new RegExp(/[^0-9.]/, "g");
     const [parentObj, valProp] = binding.expression.split('.');
     const mainData = vnode.context.$data[parentObj];
 
@@ -16,24 +16,31 @@ export default {
 
       let currency;
       let locale;
+      let minimumFractionDigits;
+      let maximumFractionDigits;
 
       if (target.value && targetValToNum !== 0) {
         if (binding.arg) {
-          if (binding.arg.length <= 3) {
+          if (!binding.arg.includes('|')) {
             currency = binding.arg
-          } else {
-            const split = binding.arg.split('[')
+          } else {   
+            const split = binding.arg.split('|')
+            const arg2 = JSON.parse(split[1].replace('(', '"').replace(')', '"'))
             currency = split[0];
-            locale = split[1];
-            locale = locale.replace(']', '')
+            [minimumFractionDigits, maximumFractionDigits, locale] = arg2;
           }
         } else {
           currency = mainData['currency'];
           locale = mainData['locale'];
+          minimumFractionDigits = mainData['minimumFractionDigits'];
+          maximumFractionDigits = mainData['maximumFractionDigits'];
         }
+
         return targetValToNum.toLocaleString(locale || navigator.language, {
           style: currency ? "currency" : undefined,
-          currency: currency ? currency : undefined
+          currency: currency ? currency : undefined,
+          minimumFractionDigits,
+          maximumFractionDigits,
         });
       }
       return '';
